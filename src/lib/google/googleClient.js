46d91daf8e8f409eb30e36e5018c7d40d8fd2e88
Carlos de Leon <admin@koxland.net>
2025-11-07 18:59:20 -0600

    Update googleClient.js

--- src/lib/google/googleClient.js
+++ src/lib/google/googleClient.js
@@ -1,6 +1,18 @@
 import { google } from 'googleapis';
 
 export function getGoogleOAuthClient() {
+  const isServiceAccount = !!process.env.GOOGLE_CLIENT_EMAIL;
+
+  if (isServiceAccount) {
+    return new google.auth.JWT(
+      process.env.GOOGLE_CLIENT_EMAIL,
+      null,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      ['https://www.googleapis.com/auth/calendar']
+    );
+  }
+
+  // Modo desarrollo (OAuth)
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
