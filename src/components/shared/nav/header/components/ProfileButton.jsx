a36be9c88a547d8ed5f44c365eed50ea148e6945
Carlos de Leon <contacto@kooxland.com>
2025-11-26 16:55:35 -0600

    Update ProfileButton.jsx

--- src/components/shared/nav/header/components/ProfileButton.jsx
+++ src/components/shared/nav/header/components/ProfileButton.jsx
@@ -6,7 +6,6 @@ import React from 'react';
 
 function ProfileButton({ role, currentUser }) {
   const router = useRouter();
-  console.log(currentUser);
   return (
     <button
       onClick={() => router.push(`/${role}/profile`)}
